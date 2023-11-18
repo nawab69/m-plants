40b80d325d51da228c17500f18189e25a6f8d7ee
nawab69 <nawabkhairuzzaman@gmail.com>
2023-11-18 15:36:15 +0600

    initital commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import fs from 'fs'
+import path from 'path'
+
+// read all json files from metadata-old folder and update the images property of each object
+const metadataPath = path.join(process.cwd(), 'metadata-old')
+const filenames = fs.readdirSync(metadataPath)
+
+const metadata = filenames.map(filename => {
+  const filePath = path.join(metadataPath, filename)
+  const fileContents = fs.readFileSync(filePath, 'utf8')
+  const { name } = path.parse(filename)
+  const { compiler, ...meta } = JSON.parse(fileContents)
+  meta.image = `https://raw.githubusercontent.com/nawab69/m-plants/main/images/${name}.png`
+  meta.properties.files[0].uri = `https://raw.githubusercontent.com/nawab69/m-plants/main/images/${name}.png`
+  return { name, meta }
+})
+
+// write all metadata as separate file in metadata folder
+const metadataFolder = path.join(process.cwd(), 'metadata')
+
+metadata.forEach(({ name, meta }) => {
+  fs.writeFileSync(
+    path.join(metadataFolder, name + '.json'),
+    JSON.stringify(meta, null, 2)
+  )
+})
